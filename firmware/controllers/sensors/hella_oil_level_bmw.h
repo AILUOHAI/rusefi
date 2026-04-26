3e3587941aa359ceb9d10e07d79e920823eac063
AILUOHAI <info@ailuohai-tech.com>
2026-04-26 05:20:45 +0000

    Fix hella_oil_level_bmw.h: remove unused getter declarations

--- firmware/controllers/sensors/hella_oil_level_bmw.h
+++ firmware/controllers/sensors/hella_oil_level_bmw.h
@@ -2,21 +2,3 @@
 
 void initHellaOilLevelSensor(bool isFirstTime);
 void deInitHellaOilLevelSensor();
-
-#if EFI_HELLA_OIL_BMW
-float getHellaOilLevelMm();
-float getHellaOilTempC();
-bool  isHellaOilLevelValid();
-bool  isHellaOilTempValid();
-uint32_t getHellaOilLevelRawUs();
-uint32_t getHellaOilTempRawUs();
-#else
-void   initHellaOilLevelSensor(bool);
-void   deInitHellaOilLevelSensor();
-float  getHellaOilLevelMm();
-float  getHellaOilTempC();
-bool   isHellaOilLevelValid();
-bool   isHellaOilTempValid();
-uint32_t getHellaOilLevelRawUs();
-uint32_t getHellaOilTempRawUs();
-#endif
